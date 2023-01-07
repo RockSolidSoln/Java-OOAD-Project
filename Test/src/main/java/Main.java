2fc987a81f9525ad8522de014218fa2d62271374
Ayaan Ahmad <ayaanishere08@gmail.com>
2023-01-07 19:03:27 +0800

    Admin dashboard functional

--- Test/src/main/java/Main.java
+++ Test/src/main/java/Main.java
@@ -2,11 +2,18 @@ import model.LoginModel;
 import view.LoginView;
 import controller.LoginController;
 
+import controller.AdminDashboardController;
+import view.AdminDashboardView;
+
+
 public class Main {
     public static void main(String[] args) {
-        LoginModel model = new LoginModel();
-        LoginView view = new LoginView();
-        LoginController controller = new LoginController(view, model);
+//        LoginModel model = new LoginModel();
+//        LoginView view = new LoginView();
+//        LoginController controller = new LoginController(view, model);
+
+        AdminDashboardView view = new AdminDashboardView();
+        AdminDashboardController controller = new AdminDashboardController(view);
 
         view.setVisible(true);
     }
